45fcb907fc3659753bcc74935181d4e3aa6fa4db
shashanktiwari <theblackboard.light@gmail.com>
2026-03-20 13:53:20 +0530

    chore: init GymLens Spring Boot project

---
+++ gymlens/src/main/java/com/bel/gymlens/GymlensApplication.java
@@ -0,0 +1,13 @@
+package com.bel.gymlens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymlensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymlensApplication.class, args);
+	}
+
+}
---
+++ gymlens/src/test/java/com/bel/gymlens/GymlensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bel.gymlens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymlensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
